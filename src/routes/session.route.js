b133a18cf43377f937139397c2daa60296992aa3
luised2301 <luised2301@gmail.com>
2022-10-27 20:14:03 -0500

    console.logs deleted

--- src/routes/session.route.js
+++ src/routes/session.route.js
@@ -6,10 +6,6 @@ const {
   createNewSession,
   getSessionsWhitPhotographerId,
   getSessionsWhitUserId,
-  // getService,
-  // editService,
-  // removeService,
-  // getAllServices,
 } = require("../usecases/sessions.usecase");
 
 const router = express.Router();
@@ -77,80 +73,5 @@ router.get("/userId/:id", async (request, response) => {
     });
   }
 });
-
-// router.get("/:id", async (request, response) => {
-//   try {
-//     const { params } = request;
-//     const service = await getService(params.id);
-//     response.json({
-//       success: true,
-//       data: {
-//         service,
-//       },
-//     });
-//   } catch (error) {
-//     response.status(400);
-//     response.json({
-//       success: false,
-//       message: error.massage,
-//     });
-//   }
-// });
-
-// router.get("/", async (request, response) => {
-//   try {
-//     const { params } = request;
-//     const services = await getAllServices();
-//     response.json({
-//       success: true,
-//       data: {
-//         services,
-//       },
-//     });
-//   } catch (error) {
-//     response.status(400);
-//     response.json({
-//       success: false,
-//       message: error.massage,
-//     });
-//   }
-// });
-
-// router.patch("/:id", async (request, response) => {
-//   try {
-//     const { params, body } = request;
-//     const service = await editService(params.id, body);
-//     response.json({
-//       success: true,
-//       data: {
-//         service,
-//       },
-//       message: "Successfully edited service",
-//     });
-//   } catch (error) {
-//     response.status(400);
-//     response.json({
-//       success: false,
-//       message: error.message,
-//     });
-//   }
-// });
-
-// router.delete("/:id", async (request, response) => {
-//   try {
-//     const { params } = request;
-//     const photo = await removeService(params.id);
-//     response.json({
-//       success: true,
-//       message: "Service deleted successfully",
-//     });
-//   } catch (error) {
-//     response.status(400);
-//     response.json({
-//       success: false,
-//       message: error.message,
-//     });
-//   }
-// });
 
 module.exports = router;
